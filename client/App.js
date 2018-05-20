2a4c1a69dc808dfa87f39c3a531dfeba0540324e
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-20 16:27:06 +0200

    add constructor and render layout

--- client/App.js
+++ client/App.js
@@ -8,3 +8,43 @@ import UsersList from './UsersList';
 import UserForm from './UserForm';
 
 const socket = io('/');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {users: [], messages: [], text: '', name: ''};
+  }
+
+  render() {
+    return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+  }
+
+  // styles.App vs className = 'name' ?
+  renderLayout() {
+    return (
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <div className={styles.AppTitle}>
+            ChatApp
+          </div>
+          <div className={styles.AppRoom}>
+            App room
+          </div>
+        </div>
+        <div className={styles.AppBody}>
+          <UsersList
+            users={this.state.users}
+          />
+          <div className={styles.MessageWrapper}>
+            <MessageList
+              messages={this.state.messages}
+            />
+            <MessageForm
+              onMessageSubmit={message => this.handleMessageSubmit(message)}
+              name={this.state.name}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
