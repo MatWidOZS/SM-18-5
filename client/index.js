2a3a0b805909cea59356fe125c2d8eabb6d74c51
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-20 14:43:41 +0200

    import react hot loader

---
+++ client/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './containers/App'
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+// dlaczego nie hot?
+
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NewApp = require('./App').default;
+    render(NewApp)
+  });
+}
